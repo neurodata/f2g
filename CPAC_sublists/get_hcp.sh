549e0034b66253a8f332c5d862d4aa5a5a1a2137
eric <eric@braincloud1.cs.jhu.edu>
2016-07-21 18:13:29 -0400

    removed old things and added new

--- CPAC_sublists/get_hcp.sh
+++ CPAC_sublists/get_hcp.sh
@@ -11,12 +11,12 @@ while read -r sub; do
     rlfile=$rlpath/rest_rl.nii.gz
     lrfile=$lrpath/rest_lr.nii.gz
     anatfile=$anatpath/anat.nii.gz
-    echo "
+    
     mkdir -p $rlpath $lrpath $anatpath
     s3cmd get s3://hcp-openaccess/HCP/$sub/unprocessed/3T/tfMRI_LANGUAGE_RL/${sub}_3T_tfMRI_LANGUAGE_RL.nii.gz $rlfile
     s3cmd get s3://hcp-openaccess/HCP/$sub/unprocessed/3T/tfMRI_LANGUAGE_LR/${sub}_3T_tfMRI_LANGUAGE_LR.nii.gz $lrfile
     s3cmd get s3://hcp-openaccess/HCP/$sub/unprocessed/3T/T1w_MPR1/${sub}_3T_T1w_MPR1.nii.gz $anatfile
-    "
+    
     echo "-" >> hcp_sublist.yml
     echo "    subject_id: '$sub'" >> hcp_sublist.yml
     echo "    unique_id: 'RL'" >> hcp_sublist.yml
